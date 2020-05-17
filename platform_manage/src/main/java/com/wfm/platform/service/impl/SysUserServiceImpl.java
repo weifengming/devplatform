3bf61909e4189602215b5701ad23cd0d7efb06c4
weifengming <15611784834@163.com>
2020-05-17 10:33:43 +0800

    1 升级mysql5 -》 mysql8 2 添加war包打包方式并去除jetty 3 镜像打包部署

--- platform_manage/src/main/java/com/wfm/platform/service/impl/SysUserServiceImpl.java
+++ platform_manage/src/main/java/com/wfm/platform/service/impl/SysUserServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 
 @Service
