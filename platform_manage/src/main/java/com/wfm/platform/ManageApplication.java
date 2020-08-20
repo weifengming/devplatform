7111ac61b3ad07b561b91d870c1f97d397809f97
weifengming <weifengming@css.com.cn>
2020-08-20 23:41:02 +0800

    去除无用代码

--- platform_manage/src/main/java/com/wfm/platform/ManageApplication.java
+++ platform_manage/src/main/java/com/wfm/platform/ManageApplication.java
@@ -1,19 +1,8 @@
 package com.wfm.platform;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * @author Weifengming
@@ -25,6 +14,6 @@ import java.time.format.DateTimeFormatter;
 public class ManageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseApplication.class,args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 }
